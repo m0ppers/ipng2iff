6947deb8c0e33960c35a97f1328217324efcff17
Andreas Streichardt <andreas.streichardt@gmail.com>
2019-10-28 07:56:24 +0100

    Add proper CLI magic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::error::Error;
 use std::fs::File;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 mod iffimage;
 
+#[derive(StructOpt, Debug)]
+#[structopt(about = "A command line utility to convert indexed PNGs to Amiga readable IFF files")]
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    infile: PathBuf,
+    #[structopt(parse(from_os_str))]
+    outfile: PathBuf,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let iff = iffimage::IffImage::from_png_file("test.png")?;
-    let mut buffer = File::create("test.iff")?;
+    let opt = Opt::from_args();
+    let iff = iffimage::IffImage::from_png_file(opt.infile)?;
+    let mut buffer = File::create(opt.outfile)?;
     iff.write(&mut buffer)?;
 
     Ok(())
