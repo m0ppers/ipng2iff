d58c812a0227642c06ba1b1c154fefa43c95c974
Andreas Streichardt <andreas.streichardt@gmail.com>
2019-10-28 07:39:49 +0100

    tests, fix pixel loop

--- src/iffimage.rs
+++ src/iffimage.rs
@@ -136,7 +136,6 @@ impl IffImage {
         if num_colors > 255 {
             return Err(From::from(IffConvertError::TooManyColors(num_colors)));
         }
-        println!("HAE {}", num_colors);
 
         let bitplanes = (num_colors as f32).log2().ceil() as u8;
         let cmap = ColorMap {
@@ -154,8 +153,8 @@ impl IffImage {
         let mut buf = vec![0; info.buffer_size()];
         reader.next_frame(&mut buf)?;
 
-        // hmm all suboptimal with this result
-        let mut pixels =
+        // hmm this nested result is really suboptimal...need an early return
+        let pixels =
             buf.chunks(3).map(|pixel| {
                 match cmap.colors.iter().position(|color| {
                     pixel[0] == color.r && pixel[1] == color.g && pixel[2] == color.b
@@ -174,7 +173,6 @@ impl IffImage {
         }
 
         let pixels = pixels.map(|pixel| pixel.unwrap()).collect::<Vec<_>>();
-        println!("PIXELS {}", pixels.len());
 
         Ok(IffImage {
             bmhd: BitmapHeader {
@@ -232,13 +230,23 @@ impl IffImage {
 
         let mut row_pixel_index = 0;
         // UFF...SMEEELLLLLSSSS!!!
-        for y in 0..self.bmhd.height {
+        for _y in 0..self.bmhd.height {
             for bpl in 0..self.bmhd.bitplanes {
                 let mut pixel_index = row_pixel_index;
                 for row_index in 0..self.bmhd.width / 8 {
                     let mut value = 0u8;
                     for bit in 0..8 {
-                        value |= ((self.pixels[pixel_index]) & (bpl + 1)) << (7 - bit);
+                        // println!(
+                        //     "pixel: {} {} {}",
+                        //     self.pixels[pixel_index],
+                        //     2i32.pow(bpl as u32),
+                        //     ((self.pixels[pixel_index]) & (2 ^ (bpl + 1)))
+                        // );
+                        if ((self.pixels[pixel_index]) & 2i32.pow(bpl as u32) as u8) != 0 {
+                            // 7-bit because of big endian
+                            value |= 1 << (7 - bit);
+                        }
+                        // println!("Value: {:b}", value);
                         pixel_index += 1;
                     }
                     row[row_index as usize] = value;
@@ -268,5 +276,65 @@ impl IffImage {
         v.extend_from_slice(&(body.len() as u32).to_be_bytes());
         v.extend_from_slice(&body);
         v
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn one_bitplanes_body() {
+        let image = IffImage {
+            bmhd: BitmapHeader {
+                width: 8,
+                height: 1,
+                bitplanes: 1,
+                ..Default::default()
+            },
+            pixels: vec![0, 1, 0, 1, 0, 1, 0, 1],
+            cmap: {
+                ColorMap {
+                    colors: vec![
+                        Color { r: 0, g: 0, b: 0 },
+                        Color {
+                            r: 0xff,
+                            g: 0xff,
+                            b: 0xff,
+                        },
+                    ],
+                }
+            },
+            ..Default::default()
+        };
+        let body = image.get_body();
+        assert_eq!(body.len(), 1);
+        assert_eq!(body[0], 0b1010101);
+    }
+
+    #[test]
+    fn two_bitplanes_body() {
+        let image = IffImage {
+            bmhd: BitmapHeader {
+                width: 8,
+                height: 1,
+                bitplanes: 2,
+                ..Default::default()
+            },
+            pixels: vec![0, 1, 2, 2, 1, 0, 0, 1],
+            cmap: {
+                ColorMap {
+                    colors: vec![
+                        Color { r: 0, g: 0, b: 0 },
+                        Color { r: 0, g: 0, b: 0 },
+                        Color { r: 0, g: 0, b: 0 },
+                    ],
+                }
+            },
+            ..Default::default()
+        };
+        let body = image.get_body();
+        assert_eq!(body.len(), 2);
+        assert_eq!(body[0], 0b01001001);
+        assert_eq!(body[1], 0b00110000);
     }
 }
